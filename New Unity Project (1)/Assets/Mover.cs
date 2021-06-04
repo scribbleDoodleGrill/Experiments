17a0d64af18a50bf8f81d070192741b50ed0126a
scribbleDoodleGrill <amethyst@legiondomain.com>
2021-06-04 14:49:52 +0800

    using time.time

--- New Unity Project (1)/Assets/Mover.cs
+++ New Unity Project (1)/Assets/Mover.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    float xValue = 0.1f;
-    float yValue = 0.01f;
-    float zValue = 0.01f;
-
-    // Start is called before the first frame update
-    void Start()
+     
+   [SerializeField] float moveSpeed = 100f;
+      void Start()
     {
+       PrintInstructions();
        
     }
+
+    
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue,yValue,zValue);
+        MovePlayer();
     }
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
+    {
+        float zValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float xValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        
+        transform.Translate(xValue,0,zValue);
+    }
+    
+
 }
---
+++ New Unity Project (1)/Assets/Scorer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    int hits = 0;
+
+
+   private void OnCollisionEnter(Collision other) 
+   {
+       hits++;
+       Debug.Log("You've bumped into this thing many times: " + hits);
+       
+   }
+}
