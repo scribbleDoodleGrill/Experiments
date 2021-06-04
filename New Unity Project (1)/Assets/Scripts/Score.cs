9efe83bc794a8156b1ca92b14544f574447be701
scribbleDoodleGrill <amethyst@legiondomain.com>
2021-06-04 18:56:02 +0800

    Spinner and Roller

--- New Unity Project (1)/Assets/Scripts/Score.cs
+++ New Unity Project (1)/Assets/Scripts/Score.cs
@@ -6,8 +6,12 @@ public class Score : MonoBehaviour
 {
     int hits = 0;
 
-    private void OnCollisionEnter(Collision other) {
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.tag !="Hit")
+        {
         hits++;
         Debug.Log("You bump this many times: " + hits);
+        }
     }
 }
