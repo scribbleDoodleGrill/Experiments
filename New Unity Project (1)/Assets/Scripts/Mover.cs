2658f0509641187efaf69c16f98c90b335fb645e
scribbleDoodleGrill <amethyst@legiondomain.com>
2021-06-05 07:30:41 +0800

    obstacle course done

--- New Unity Project (1)/Assets/Scripts/Mover.cs
+++ New Unity Project (1)/Assets/Scripts/Mover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
      
-   [SerializeField] float moveSpeed = 100f;
+   [SerializeField] float moveSpeed = 500f;
       void Start()
     {
        PrintInstructions();
