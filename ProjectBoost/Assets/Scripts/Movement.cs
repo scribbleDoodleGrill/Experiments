343d510adde8212062ad0dafea8cf2c240ce2115
scribbleDoodleGrill <amethyst@legiondomain.com>
2021-06-05 14:43:43 +0800

    Rocket Boost

---
+++ ProjectBoost/Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        ProcessThrust();
+    }
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Pressed SPACE - Thrusting");
+        }
+       
+
+
+    }
+    void ProcessRotation()
+    {
+         if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Rotating Right");
+        }
+    }
+
+}
