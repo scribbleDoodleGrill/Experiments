b0c896cf19663ded3533a6bb77ee01b8e0c96cf2
scribbleDoodleGrill <amethyst@legiondomain.com>
2021-06-07 08:57:09 +0800

    SceneManager

--- ProjectBoost/Assets/Scripts/CollisionHandler.cs
+++ ProjectBoost/Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CollisionHandler : MonoBehaviour
 {
-    
-    void Start()
+    void OnCollisionEnter(Collision other)
+
     {
-        
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                 Debug.Log("Congrats you finished");
+                break;
+            case "Fuel":
+                Debug.Log("You picked up a fuel");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+
+
+
+
     }
 
-    
-    void Update()
-    {
-        
+    void ReloadLevel ()
+       {
+        SceneManager.LoadScene(0);
     }
 }
