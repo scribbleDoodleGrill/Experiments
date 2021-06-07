ebd9e10053c54c9c69526cb3b7601358faa556d5
scribbleDoodleGrill <amethyst@legiondomain.com>
2021-06-07 16:32:36 +0800

    okay with the scene changing

--- ProjectBoost/Assets/Scripts/Movement.cs
+++ ProjectBoost/Assets/Scripts/Movement.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationThrust = 1f;
     
+    Rigidbody rb;
+    AudioSource audioSource;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,20 +30,42 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Pressed Space -  Thrusting");
+              rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+               if(!audioSource.isPlaying)
+               {
+                audioSource.Play(); 
+               }
+             
         }
+           else
+        {
+            audioSource.Stop();        
+        } 
+               
+        
+    
+        
         
     }
 
      void ProcessRotation()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-           Debug.Log("Left");
+         if (Input.GetKey(KeyCode.A))
+         {
+         ApplyRotation(rotationThrust);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-           Debug.Log("Right");
+           ApplyRotation(-rotationThrust);
         }
     }
+
+     void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+    
+
 }
