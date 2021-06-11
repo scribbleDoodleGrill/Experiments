d35aa9e8afede7d5dbee7608b01b4d26b5a9b5c5
scribbleDoodleGrill <amethyst@legiondomain.com>
2021-06-11 12:32:46 +0800

    mathf.clamp

--- Argony Assault/Assets/Scripts/PlayerControls.cs
+++ Argony Assault/Assets/Scripts/PlayerControls.cs
@@ -1,26 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 
 public class PlayerControls : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 3.5f;
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
-    //    float horizontalThrow = Input.GetAxis("Horizontal");
-    //    Debug.Log(horizontalThrow);
 
-    //    float verticalThrow = Input.GetAxis("Verticak");
-    //    Debug.Log(verticalThrow);
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+       
+       float xOffset = xThrow * Time.deltaTime * controlSpeed;
+       float rawXPos = transform.localPosition.x + xOffset;
+       float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+       float yOffset = yThrow * Time.deltaTime * controlSpeed;
+       float rawYPos = transform.localPosition.y + yOffset;
+       float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3 
+        (clampedXPos, clampedYPos, transform.localPosition.z);
+
     }
 }
