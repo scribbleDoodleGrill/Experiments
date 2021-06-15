aff8492b671ad009c2d7ef693dde08c7f444e9da
scribbleDoodleGrill <amethyst@legiondomain.com>
2021-06-15 09:47:10 +0800

    Enemy Hit Points

--- Argony Assault/Assets/Scripts/Enemy.cs
+++ Argony Assault/Assets/Scripts/Enemy.cs
@@ -6,12 +6,40 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 2;
 
+    ScoreBoard scoreBoard;
 
-    void OnParticleCollision(GameObject other)
+    void Start() 
     {
-            GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+
+   void OnParticleCollision(GameObject other)
+    {
+            ProcessHit();
+            if (hitPoints <1)
+            {
+                KillEnemy();
+            }
+    }
+
+    void ProcessHit()
+    {
+        hitPoints--;
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy ()
+    {
+          GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
             vfx.transform.parent = parent;
             Destroy(gameObject);
     }
+
+
+
+
 }
