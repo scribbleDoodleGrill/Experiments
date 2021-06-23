b7a645de9ddf8add39493c943dad95fda07175bf
scribbleDoodleGrill <amethyst@legiondomain.com>
2021-06-23 10:34:59 +0800

    done

--- Realm Rush/New Unity Project/Assets/Scripts/Waypoint.cs
+++ Realm Rush/New Unity Project/Assets/Scripts/Waypoint.cs
@@ -9,7 +9,26 @@ public class Waypoint : MonoBehaviour
     [SerializeField] bool isPlaceable;
     public bool IsPlaceable  {  get {  return isPlaceable; } }
 
+    GridManager gridManager;
+    Vector2Int coordinates = new Vector2Int();
+
+   void Awake() 
+      {
+        gridManager = FindObjectOfType<GridManager>();
+      }
     
+    void Start() 
+    {
+      if(gridManager != null)
+      {
+        coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+        
+        if(!isPlaceable)
+        {
+          gridManager.BlockNode(coordinates);
+        }
+      }
+    }
     void OnMouseDown() 
     {
       if (isPlaceable)
