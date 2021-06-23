96d5db7b4e2fb5b18b2487fe8d0f3f06deaee448
scribbleDoodleGrill <amethyst@legiondomain.com>
2021-06-23 19:33:20 +0800

    raycasting

--- Zombie Runner/zombie runner/Assets/Scripts/EnemyAI.cs
+++ Zombie Runner/zombie runner/Assets/Scripts/EnemyAI.cs
@@ -7,16 +7,64 @@ public class EnemyAI : MonoBehaviour
 {
 
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+     
      NavMeshAgent navMeshAgent;
-    // Start is called before the first frame update
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
+    
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if(isProvoked)
+        {
+            EngageTarget();
+    
+        }
+        else if(distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+
+           
+        }
+        
+    }
+
+    void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+
+    void ChaseTarget()
+    {
+         navMeshAgent.SetDestination(target.position);
+        
+    }
+
+    void AttackTarget()
+    {
+        Debug.Log(name + "has seeked and is destroying" + target.name);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
